5605b79bd8d8187f22ff70d2bd4e69ca02c22802
Sahil Atahar <100127570+Sahil-Atahar@users.noreply.github.com>
2022-09-08 14:58:27 +0530

    Add files via upload

---
+++ If Else Statement/If Else Statement/leap_year.cpp
@@ -0,0 +1,22 @@
+﻿//Leap Year
+//Check whether a year is a leap year or not.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int year;
+	
+	cout << "Enter a number: ";
+	cin >> year;
+	
+	if(year % 400 == 0 ) {
+		cout << year << " is a leap year.";
+				} else if(year % 100 == 0 ) {
+		cout << year << " is not a leap year.";
+				} else if(year % 4 == 0 ) {
+		cout << year << " is a leap year.";
+				} 
+				
+		return 0;
+	}
