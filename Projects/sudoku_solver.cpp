deaf99b5d5880ff2d25471e10a37171e0cd99337
Sahil Atahar <sahilatahar@gmail.com>
2022-12-30 12:02:51 +0530

    Sudoku solver added

--- Projects/sudoku_solver.cpp
+++ Projects/sudoku_solver.cpp
@@ -1,154 +1,113 @@
 #include <iostream>
-
+#include <conio.h>
 using namespace std;
 
-// N is the size of the 2D matrix N*N
-#define N 9
+#define totalRows 9
+#define totaColumns 9
 
-/* A utility function to print grid */
-void print(int arr[N][N])
+bool isCorectPlace(int sudoku[totalRows][totaColumns], int row, int col, int num)
 {
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-			cout << arr[i][j] << " ";
-		cout << endl;
-	}
+    // checking row
+    for (int i = 0; i < totalRows; i++)
+    {
+        if (sudoku[row][i] == num)
+            return false;
+    }
+
+    // cheking column
+    for (int i = 0; i < totaColumns; i++)
+    {
+        if (sudoku[i][col] == num)
+            return false;
+    }
+
+    // checking grid
+    int rowStarting = row >= 6 ? 6 : (row >= 3 ? 3 : 0);
+    int colStarting = col >= 6 ? 6 : (col >= 3 ? 3 : 0);
+    for (int i = rowStarting; i < rowStarting + 3; i++)
+    {
+        for (int j = colStarting; j < colStarting + 3; j++)
+        {
+            if (sudoku[i][j] == num)
+                return false;
+        }
+    }
+
+    return true;
 }
 
-// Checks whether it will be
-// legal to assign num to the
-// given row, col
-bool isSafe(int grid[N][N], int row,
-			int col, int num)
+bool solveSudoku(int sudoku[totalRows][totaColumns], int row, int col)
 {
+    // if all box visited then return true
+    if (row == totalRows - 1 && col == totaColumns)
+        return true;
 
-	// Check if we find the same num
-	// in the similar row , we
-	// return false
-	for (int x = 0; x <= 8; x++)
-		if (grid[row][x] == num)
-			return false;
+    // if column reached at last column then goto next row
+    if (col == totaColumns)
+    {
+        row++;
+        col = 0;
+    }
 
-	// Check if we find the same num in
-	// the similar column , we
-	// return false
-	for (int x = 0; x <= 8; x++)
-		if (grid[x][col] == num)
-			return false;
+    // if box is filled then check next box of column
+    if (sudoku[row][col] > 0)
+        return solveSudoku(sudoku, row, col + 1);
 
-	// Check if we find the same num in
-	// the particular 3*3 matrix,
-	// we return false
-	int startRow = row - row % 3,
-		startCol = col - col % 3;
-
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			if (grid[i + startRow][j +
-								   startCol] == num)
-				return false;
-
-	return true;
+    // put 1 to 9 in box and checking conditions
+    for (int num = 1; num <= 9; num++)
+    { // checking conditions
+        if (isCorectPlace(sudoku, row, col, num))
+        {
+            sudoku[row][col] = num;
+            // if value filled in box then move to next box
+            if (solveSudoku(sudoku, row, col + 1))
+                return true;
+        }
+        sudoku[row][col] = 0;
+    }
+    return false;
 }
 
-/* Takes a partially filled-in grid and attempts
-to assign values to all unassigned locations in
-such a way to meet the requirements for
-Sudoku solution (non-duplication across rows,
-columns, and boxes) */
-bool solveSudoku(int grid[N][N], int row, int col)
-{
-	// Check if we have reached the 8th
-	// row and 9th column (0
-	// indexed matrix) , we are
-	// returning true to avoid
-	// further backtracking
-	if (row == N - 1 && col == N)
-		return true;
-
-	// Check if column value becomes 9 ,
-	// we move to next row and
-	// column start from 0
-	if (col == N)
-	{
-		row++;
-		col = 0;
-	}
-
-	// Check if the current position of
-	// the grid already contains
-	// value >0, we iterate for next column
-	if (grid[row][col] > 0)
-		return solveSudoku(grid, row, col + 1);
-
-	for (int num = 1; num <= N; num++)
-	{
-
-		// Check if it is safe to place
-		// the num (1-9) in the
-		// given row ,col ->we
-		// move to next column
-		if (isSafe(grid, row, col, num))
-		{
-
-			/* Assigning the num in
-				the current (row,col)
-				position of the grid
-				and assuming our assigned
-				num in the position
-				is correct	 */
-			grid[row][col] = num;
-
-			// Checking for next possibility with next
-			// column
-			if (solveSudoku(grid, row, col + 1))
-				return true;
-		}
-
-		// Removing the assigned num ,
-		// since our assumption
-		// was wrong , and we go for
-		// next assumption with
-		// diff num value
-		grid[row][col] = 0;
-	}
-	return false;
-}
-
-// Driver Code
 int main()
 {
-	// 0 means unassigned cells
-	int grid[N][N];
-	//  = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
-	//                   {5, 2, 0, 0, 0, 0, 0, 0, 0},
-	//                   {0, 8, 7, 0, 0, 0, 0, 3, 1},
-	//                   {0, 0, 3, 0, 1, 0, 0, 8, 0},
-	//                   {9, 0, 0, 8, 6, 3, 0, 0, 5},
-	//                   {0, 5, 0, 0, 9, 0, 6, 0, 0},
-	//                   {1, 3, 0, 0, 0, 0, 2, 5, 0},
-	//                   {0, 0, 0, 0, 0, 0, 0, 7, 4},
-	//                   {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+    int sudoku[totalRows][totaColumns];
+    // Example values to check
+    //      {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+    //       {5, 2, 0, 0, 0, 0, 0, 0, 0},
+    //       {0, 8, 7, 0, 0, 0, 0, 3, 1},
+    //       {0, 0, 3, 0, 1, 0, 0, 8, 0},
+    //       {9, 0, 0, 8, 6, 3, 0, 0, 5},
+    //       {0, 5, 0, 0, 9, 0, 6, 0, 0},
+    //       {1, 3, 0, 0, 0, 0, 2, 5, 0},
+    //       {0, 0, 0, 0, 0, 0, 0, 7, 4},
+    //       {0, 0, 5, 2, 0, 6, 3, 0, 0}};
 
-	cout << "Enter the Sudoku number enter 0 (zero) in place of empty: \n";
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			cin >> grid[i][j];
-		}
-	}
+    cout << "Enter the Sudoku number.\nEnter 0 (zero) in place of empty: \n";
+    for (int i = 0; i < totalRows; i++)
+    {
+        for (int j = 0; j < totaColumns; j++)
+        {
+            cin >> sudoku[i][j];
+        }
+    }
+    cout << endl;
 
-	cout << endl
-		 << endl
-		 << endl;
-
-	if (solveSudoku(grid, 0, 0))
-		print(grid);
-	else
-		cout << "no solution exists " << endl;
-
-	return 0;
-	// This is code is contributed by Pradeep Mondal P
+    if (solveSudoku(sudoku, 0, 0))
+    {
+        cout << "Solved Sudoku is here : \n";
+        for (int i = 0; i < totalRows; i++)
+        {
+            for (int j = 0; j < totaColumns; j++)
+            {
+                cout << sudoku[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << "No solution exists";
+    }
+    getch();
+    return 0;
 }
